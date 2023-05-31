2c7edbe1dd6e1eb13aad34bfe0954763de79796a
John Wason <wason@wasontech.com>
2023-05-31 04:18:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Simple Launch Client
+
+This library contains client utility functions for processes launched using `simple-launch`, although they may also be used without `simple-launch`. Currently this package provides a reliable way for processes to receive shutdown signals from a process manager or the user using `ctrl-c`.
+
+See `simple-launch`: https://github.com/johnwason/simple-launch
+
+## Installation
+
+```
+python -m pip install --user simple-launch-process
+```
+
+On Ubuntu, it may be necessary to replace `python` with `python3`.
+
+## Usage
+
+The module contains three functions:
+
+* `simple_launch_process.wait_exit()` - Block and wait for exit
+* `simple_launch_process.wait_exit_callback(callback)` - Return immediately and call `callback()` when shutdown signal received
+* `simple_launch_process.wait_exit_stop_loop(loop)` - Call to stop an `asyncio` loop when shutdown signal received
+
+## Example
+
+A simple example of running a Python http server in a background thread, using the blocking `wait_exit()`.
+
+```python
+import simple_launch_process
+import threading
+from http import server as http_server
+
+server=http_server.ThreadingHTTPServer(('',8085), 
+        http_server.SimpleHTTPRequestHandler)
+def run_server():
+    server.serve_forever()
+
+th = threading.Thread(target=run_server)
+th.daemon = True
+th.start()
+
+simple_launch_process.wait_exit()
+```
+
+This example uses the callback version to quit the http server:
+
+```python
+import simple_launch_process
+import threading
+from http import server as http_server
+
+server=http_server.ThreadingHTTPServer(('',8085), 
+        http_server.SimpleHTTPRequestHandler)
+
+def shutdown_server():
+    server.shutdown()
+
+simple_launch_process.wait_exit_callback(shutdown_server)
+
+server.serve_forever()
+```
+
+## Shutdown Signal Explanation
+
+Reliably sending a shutdown/quit command to a process in a cross-platform manner is surprisingly difficult. On POSIX based systems like Linux and Mac OS X, signals such as `SIGINT` or `SIGTERM` are typically sent. (Ctrl-C sends `SIGINT`). These signals can be caught by the process, and used to gracefully shut down. `SIGKILL` is used to immediately terminate the process.
+
+On Windows, it is significantly more difficult. Windows typically uses "Window Message Queues" to communicate with processes. Even processes like services that do not have a visible window often have a hidden window so messages can be received from the operating system. There is some functionality for sending console signals, but these are not reliable for all cases. Windows also has the concept of `Job Objects` that can be used to group different processes together. The combination of job objects and windows messages provide a reliable way to send and receive graceful shutdown signals.
+
+The `wait_for_exit*` commands either way for Signals on POSIX, or windows close messages and console events on Windows.
+
+
---
+++ simple_launch_process.py
@@ -0,0 +1,139 @@
+import sys
+import atexit
+import traceback
+import threading
+
+def wait_exit():
+
+    #Wait for shutdown signal if running in service mode
+    print("Press Ctrl-C to quit...")
+    if sys.platform == "win32":
+        _win32_wait_exit()
+    else:
+        import signal
+        signal.sigwait([signal.SIGTERM,signal.SIGINT])
+    
+def wait_exit_callback(callback):
+    def t_func():
+        try:
+            
+            #Wait for shutdown signal if running in service mode
+            print("Press Ctrl-C to quit...")
+            if sys.platform == "win32":
+                hwnd = _win32_create_message_hwnd()
+                def _stop_loop():
+                    _win32_post_hwnd_close(hwnd)
+                try:
+                    atexit.register(_stop_loop)
+                    _win32_wait_message_hwnd(hwnd)
+                finally:
+                    try:
+                        atexit.unregister(_stop_loop)
+                    except Exception: pass
+            else:
+                import signal
+                signal.sigwait([signal.SIGTERM,signal.SIGINT])
+        except Exception:
+            traceback.print_exc()
+        callback()
+    
+    t = threading.Thread(target=t_func)
+    t.setDaemon(True)
+    t.start()
+
+def wait_exit_stop_loop(loop): 
+    wait_exit_callback(lambda: loop.call_soon_threadsafe(loop.stop))
+
+if sys.platform == "win32":
+    # https://gist.github.com/mouseroot/6128651
+    import ctypes
+    import ctypes.wintypes
+
+    WNDPROCTYPE = ctypes.WINFUNCTYPE(ctypes.c_int, ctypes.wintypes.HWND, ctypes.c_uint, ctypes.wintypes.WPARAM, ctypes.wintypes.LPARAM)
+    CtrlCHandlerRoutine = ctypes.WINFUNCTYPE(ctypes.wintypes.BOOL, ctypes.wintypes.DWORD)
+    WM_DESTROY = 2
+    WM_CLOSE = 16
+    HWND_MESSAGE = ctypes.wintypes.HWND(-3)
+
+    class WNDCLASSEX(ctypes.Structure):
+        _fields_ = [("cbSize", ctypes.c_uint),
+                    ("style", ctypes.c_uint),
+                    ("lpfnWndProc", WNDPROCTYPE),
+                    ("cbClsExtra", ctypes.c_int),
+                    ("cbWndExtra", ctypes.c_int),
+                    ("hInstance", ctypes.wintypes.HANDLE),
+                    ("hIcon", ctypes.wintypes.HANDLE),
+                    ("hCursor", ctypes.wintypes.HANDLE),
+                    ("hBrush", ctypes.wintypes.HANDLE),
+                    ("lpszMenuName", ctypes.wintypes.LPCWSTR),
+                    ("lpszClassName", ctypes.wintypes.LPCWSTR),
+                    ("hIconSm", ctypes.wintypes.HANDLE)]
+
+    def _PyWndProcedure(hWnd, Msg, wParam, lParam):
+        if Msg == WM_DESTROY:
+            ctypes.windll.user32.PostQuitMessage(0)
+        elif Msg == WM_CLOSE:
+            ctypes.windll.user32.DestroyWindow(hWnd)
+        else:
+            try:
+                # TODO: Why is this raising on error?
+                return ctypes.windll.user32.DefWindowProcW(hWnd, Msg, wParam, 0)
+            except ctypes.ArgumentError:
+                pass
+        return 0
+
+    WndProc = WNDPROCTYPE(_PyWndProcedure)
+
+    def _ctrl_c_empty_handler(code):
+        return True
+
+    _ctrl_c_empty_handler_ptr = CtrlCHandlerRoutine(_ctrl_c_empty_handler)
+
+    def _win32_wait_exit():
+        hwnd = _win32_create_message_hwnd()
+        _win32_wait_message_hwnd(hwnd)
+
+    def _win32_create_message_hwnd():
+        
+        hInst = ctypes.windll.kernel32.GetModuleHandleW(0)
+        wclassName = 'pyri_message_window'
+        wname = 'pyri_hidden_window'
+        
+        wndClass = WNDCLASSEX()
+        wndClass.cbSize = ctypes.sizeof(WNDCLASSEX)
+        wndClass.lpfnWndProc = WndProc
+        wndClass.lpszClassName = wclassName
+        wndClass.hInstance = hInst
+
+        regRes = ctypes.windll.user32.RegisterClassExW(ctypes.byref(wndClass))
+        assert regRes, "Could not create win32 message wnd class"
+
+        hWnd = ctypes.windll.user32.CreateWindowExW( 0, wclassName, wname, 0, 0, 0, 0, 0, HWND_MESSAGE, None, None, None )
+        assert hWnd, "Could not create win32 message hwnd"
+        return hWnd
+
+    def _win32_wait_message_hwnd(hWnd):
+
+
+        # Install a ctrl-c handler to send WM_QUIT
+
+        def ctrl_c_handler(code):
+            _win32_post_hwnd_close(hWnd)
+            return True
+
+        ctrl_c_handler_ptr = CtrlCHandlerRoutine(ctrl_c_handler)
+        
+        ctypes.windll.kernel32.SetConsoleCtrlHandler(ctrl_c_handler_ptr, 1)
+        
+        msg = ctypes.wintypes.MSG()
+        lpmsg = ctypes.pointer(msg)
+        while ctypes.windll.user32.GetMessageA(lpmsg, 0, 0, 0) != 0:
+            ctypes.windll.user32.TranslateMessage(lpmsg)
+            ctypes.windll.user32.DispatchMessageA(lpmsg)
+
+        ctypes.windll.kernel32.SetConsoleCtrlHandler(_ctrl_c_empty_handler_ptr, 1)
+
+    def _win32_post_hwnd_close(hWnd):
+        ctypes.windll.user32.PostMessageW(hWnd,WM_CLOSE,0,0)
+
+__all__ = ["wait_exit","wait_exit_callback","wait_exit_stop_loop"]
